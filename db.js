d56e0c149d00672ced6f946d6a6fe236f28bf0f0
Faiyaz Sarkar02 <faiyazsarkar24@gmail.com>
2026-07-30 22:41:53 +0600

    feat(quiz): initialize quiz game logic and data schema
    - Wrap quiz features inside a main quizGame function
    - Add quiz question dataset in db.js
    - Add updateQuiz function to display questions and options by currentQuizIndex

---
+++ db.js
@@ -0,0 +1,58 @@
+export const quizData = [
+  {
+    question: "Which HTML element is used to create a hyperlink?",
+    options: ["<link>", "<a>", "<href>", "<url>"],
+    answer: "<a>",
+  },
+
+  {
+    question: "Which CSS property changes the text color?",
+    options: ["font-color", "text-color", "color", "background-color"],
+    answer: "color",
+  },
+  {
+    question: "Which HTML tag is used to display an image?",
+    options: ["<picture>", "<img>", "<image>", "<src>"],
+    answer: "<img>",
+  },
+  {
+    question: "Which JavaScript keyword is used to declare a constant?",
+    options: ["let", "var", "const", "static"],
+    answer: "const",
+  },
+  {
+    question: "Which CSS layout system is one-dimensional?",
+    options: ["Grid", "Flexbox", "Float", "Table"],
+    answer: "Flexbox",
+  },
+  {
+    question: "Which method adds an element to the end of an array?",
+    options: ["shift()", "push()", "pop()", "unshift()"],
+    answer: "push()",
+  },
+  {
+    question: "Which event occurs when a button is clicked?",
+    options: ["mouseover", "keydown", "click", "submit"],
+    answer: "click",
+  },
+  {
+    question: "Which HTML tag is used for the largest heading?",
+    options: ["<h6>", "<heading>", "<head>", "<h1>"],
+    answer: "<h1>",
+  },
+  {
+    question: "Which JavaScript method converts JSON into an object?",
+    options: [
+      "JSON.stringify()",
+      "JSON.parse()",
+      "JSON.convert()",
+      "JSON.object()",
+    ],
+    answer: "JSON.parse()",
+  },
+  {
+    question: "Which CSS property makes an element a flex container?",
+    options: ["display: grid", "display: flex", "position: flex", "flex: 1"],
+    answer: "display: flex",
+  },
+];
--- script.js
+++ script.js
@@ -1,134 +1,28 @@
 // @ts-nocheck
-const quizData = [
-  {
-    question: "Which HTML element is used to create a hyperlink?",
-    options: ["<link>", "<a>", "<href>", "<url>"],
-    answer: "<a>",
-  },
+import { quizData } from "./db.js";
 
-  {
-    question: "Which CSS property changes the text color?",
-    options: ["font-color", "text-color", "color", "background-color"],
-    answer: "color",
-  },
-  {
-    question: "Which HTML tag is used to display an image?",
-    options: ["<picture>", "<img>", "<image>", "<src>"],
-    answer: "<img>",
-  },
-  {
-    question: "Which JavaScript keyword is used to declare a constant?",
-    options: ["let", "var", "const", "static"],
-    answer: "const",
-  },
-  {
-    question: "Which CSS layout system is one-dimensional?",
-    options: ["Grid", "Flexbox", "Float", "Table"],
-    answer: "Flexbox",
-  },
-  {
-    question: "Which method adds an element to the end of an array?",
-    options: ["shift()", "push()", "pop()", "unshift()"],
-    answer: "push()",
-  },
-  {
-    question: "Which event occurs when a button is clicked?",
-    options: ["mouseover", "keydown", "click", "submit"],
-    answer: "click",
-  },
-  {
-    question: "Which HTML tag is used for the largest heading?",
-    options: ["<h6>", "<heading>", "<head>", "<h1>"],
-    answer: "<h1>",
-  },
-  {
-    question: "Which JavaScript method converts JSON into an object?",
-    options: [
-      "JSON.stringify()",
-      "JSON.parse()",
-      "JSON.convert()",
-      "JSON.object()",
-    ],
-    answer: "JSON.parse()",
-  },
-  {
-    question: "Which CSS property makes an element a flex container?",
-    options: ["display: grid", "display: flex", "position: flex", "flex: 1"],
-    answer: "display: flex",
-  },
-];
+function quizGame(params) {
+  let currentQuizIndex = 1;
 
-const question = document.querySelector(".question-card");
-const nextBtn = document.querySelector(".actions");
-const progress = document.querySelector(".progress");
-const ansOptions = document.querySelector(".options");
-const progressFillBar = document.querySelector(".progress-fill");
-let currentIndex = 0;
-let score = 0;
+  function updateQuiz(currentQuizIndex) {
+    // get access to current quiz
+    const currentQuiz = quizData[currentQuizIndex];
 
-ansOptions.addEventListener("change", (e) => {
-  if (e.target.type !== "radio") return;
-  const radios = ansOptions.querySelectorAll('input[type="radio"]');
+    // get access to elements of individual quiz in html
+    const quizQuestion = document.querySelector(".question-card h2");
+    const quizOptions = document.querySelector(".options");
 
-  radios.forEach((radio) => {
-    radio.disabled = true;
-  });
+    // assign quiz data to these following elements
+    quizQuestion.innerHTML = currentQuiz.question;
+    currentQuiz.options.forEach((option) => {
+      const li = document.createElement("li");
+      li.innerHTML = option;
+      li.classList.add("option");
+      quizOptions.appendChild(li);
+    });
+  }
 
-  const checkedRadio = ansOptions.querySelector('input[name="answer"]:checked');
-
-  [...radios].map((radio) => {
-    if (checkedRadio.value === radio.value) {
-      radio.parentElement.style.background = "rgba(205, 46, 32, 0.53)";
-    }
-    if (quizData[currentIndex].answer === radio.value) {
-      radio.parentElement.style.background = "rgba(68, 255, 0, 0.15)";
-    }
-    if (
-      checkedRadio.value === radio.value &&
-      quizData[currentIndex].answer === radio.value
-    ) {
-      score = score + 1;
-      return radio;
-    }
-    return radio;
-  });
-});
-
-function loopQuiz(index) {
-  progressFillBar.style.width = `${((index + 1) / quizData.length) * 100}%`;
-
-  progress.querySelector("span").innerHTML =
-    `Question ${index + 1} of ${quizData.length}`;
-  question.querySelector("h2").innerText = quizData[index].question;
-  ansOptions.innerHTML = "";
-  quizData[index].options.map((option) => {
-    const escaped = option.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    return (ansOptions.innerHTML += `<label class="option">
-          <input type="radio" value=${escaped}  name="answer" />
-          <span>${escaped}</span>
-        </label>`);
-  });
+  updateQuiz(currentQuizIndex);
 }
-loopQuiz(currentIndex);
 
-nextBtn.addEventListener("click", () => {
-  const checkedRadio = document.querySelector('input[name="answer"]:checked');
-  console.log(checkedRadio);
-  if (!checkedRadio?.value) {
-    question.querySelector("span").innerText =
-      "Error : please select an option";
-    console.log("please select an option");
-    return;
-  }
-
-  if (currentIndex === quizData.length - 1) {
-    console.log("quiz is done, you got", score, "out of", quizData.length);
-  } else {
-    currentIndex = currentIndex + 1;
-  }
-
-  console.log(score);
-  question.querySelector("span").innerText = "";
-
-  loopQuiz(currentIndex);
-});
+quizGame();
