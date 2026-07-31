2594b0ec2e9a1e4bd89d541fd1ec3923b17b9a92
Faiyaz Sarkar02 <faiyazsarkar24@gmail.com>
2026-07-31 13:57:56 +0600

    feat(quiz): add option selection delegation and next step control
    - Implement event delegation on options container to handle answer clicks
    - Highlight correct and incorrect choices on user selection
    - Disable Next button by default and enable only after option selection
    - Add click listener to Next button to increment question index

--- script.js
+++ script.js
@@ -3,26 +3,57 @@ import { quizData } from "./db.js";
 
 function quizGame(params) {
   let currentQuizIndex = 1;
+  const nextQuestionBtn = document.querySelector(".actions button");
+  const quizOptions = document.querySelector(".options");
 
-  function updateQuiz(currentQuizIndex) {
-    // get access to current quiz
+  renderQuiz();
+
+  // listen for option changes and handle select option
+  quizOptions.addEventListener("change", (e) => {
     const currentQuiz = quizData[currentQuizIndex];
+    document.querySelectorAll(".option input").forEach((inp) => {
+      inp.disabled = true;
+      if (inp.value === e.target.value) {
+        inp.parentElement.style.background = "#ef17032c";
+      }
+      if (inp.value === currentQuiz.answer) {
+        inp.parentElement.style.background = "#4aef0329";
+      }
+    });
 
-    // get access to elements of individual quiz in html
+    // show next question btn when option is selected
+    document.querySelector(".actions button").style.display = "block";
+  });
+
+  // listen for next question btn and handle
+  nextQuestionBtn.addEventListener("click", (e) => {
+    currentQuizIndex++;
+    renderQuiz();
+  });
+
+  function renderQuiz() {
     const quizQuestion = document.querySelector(".question-card h2");
-    const quizOptions = document.querySelector(".options");
-
+    const currentQuiz = quizData[currentQuizIndex];
     // assign quiz data to these following elements
     quizQuestion.innerHTML = currentQuiz.question;
+    quizOptions.innerHTML = "";
     currentQuiz.options.forEach((option) => {
-      const li = document.createElement("li");
-      li.innerHTML = option;
-      li.classList.add("option");
-      quizOptions.appendChild(li);
+      const label = document.createElement("label");
+      const span = document.createElement("span");
+      const input = document.createElement("input");
+      label.classList.add("option");
+      input.setAttribute("type", "radio");
+      input.setAttribute("name", "option");
+      input.setAttribute("value", option);
+      span.textContent = option;
+      label.appendChild(input);
+      label.appendChild(span);
+      quizOptions.appendChild(label);
     });
-  }
 
-  updateQuiz(currentQuizIndex);
+    // hide next question btn
+    nextQuestionBtn.style.display = "none";
+  }
 }
 
 quizGame();
